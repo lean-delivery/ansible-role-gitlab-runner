7ab926560eec3702fa02da8d3fefeb6fb0a5cb9e
Viktor Utkin <viktor.utkin7@yandex.ru>
2019-02-01 15:13:17 -0500

    role refactoring

--- molecule/resources/tests/test_default_redhat.py
+++ molecule/resources/tests/test_default_redhat.py
@@ -9,12 +9,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_gitlab_runner_package_is_installed(host):
-    gitlab_runner_pkg = host.package("gitlab-runner")
-    assert gitlab_runner_pkg.is_installed
-    assert gitlab_runner_pkg.version.startswith("10.5")
+    flag = False
+    for package in ["gitlab-runner", "gitlab-ci-multi-runner"]:
+        with host.sudo():
+            package = host.package(package)
+            if package.is_installed and package.version.startswith("10.5"):
+                flag = True
+    assert flag
 
 
 def test_gitlab_runner_service_running_and_enabled(host):
-    gitlab_runner_svc = host.service("gitlab-runner")
-    assert gitlab_runner_svc.is_running
-    assert gitlab_runner_svc.is_enabled
+    flag = False
+    for service in ["gitlab-runner", "gitlab-ci-multi-runner"]:
+        with host.sudo():
+            service = host.service(service)
+            if service.is_running and service.is_enabled:
+                flag = True
+    assert flag
