954dba749febb12f00e20a48d0f08c4986af082b
Max Kavun <Maksim_Kavun@epam.com>
2019-11-15 16:55:07 +0300

    add new filter

---
+++ filter_plugins/from_toml.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+DOCUMENTATION = '''
+---
+module: to_toml, from_toml
+version_added: "2.8"
+short_description: Converts Python data to TOML and TOML to Python data.
+author:
+    - "Samy Coenen (contact@samycoenen.be)"
+'''
+
+import toml
+
+def to_toml(data):
+    ''' Convert the value to TOML '''
+    return toml.dumps(data)
+
+def from_toml(data):
+    ''' Convert TOML to Python data '''
+    return toml.loads(data)
+
+class FilterModule(object):
+    ''' Ansible TOML jinja2 filters '''
+
+    def filters(self):
+        return {
+            # toml
+            'to_toml': to_toml,
+            'from_toml': from_toml
+        }
